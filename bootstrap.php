305a5a09aecf9bc052aa7d449ace5b630b394542
gaideliai <61122798+gaideliai@users.noreply.github.com>
2020-06-24 22:19:52 +0300

    sort accounts by surname

--- bootstrap.php
+++ bootstrap.php
@@ -7,4 +7,12 @@ $URL = 'http://localhost/uzdaviniai/bankas/';
 $data = json_decode(file_get_contents(__DIR__ .'/accounts.json'), 1);
 $loginData = json_decode(file_get_contents(__DIR__ .'/login.json'), 1);
 
-// _d($data);
+function sort_by_surname ($key) {
+    return function ($a, $b) use ($key) {       
+        return strnatcmp($a[$key], $b[$key]);
+    };
+}
+
+usort($data, sort_by_surname('surname'));
+
+_d($data);
